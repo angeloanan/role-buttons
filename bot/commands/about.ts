a3a3fbfde3b992eafcc9d840f48af2bb8527cac2
Christopher Angelo <angelo@angeloanan.xyz>
2022-02-20 05:05:56 +0700

    feat(about): initial command functionality

--- bot/commands/about.ts
+++ bot/commands/about.ts
@@ -1,8 +1,75 @@
+import { stripIndent } from 'common-tags'
+import { prisma } from 'db'
+import os from 'node:os'
+
+import { client } from '../index.js'
 import type { BotCommandHandler } from '../internals'
 
 const handler: BotCommandHandler = async interaction => {
-  await interaction.reply({
-    content: 'I am a bot!'
+  await interaction.deferReply({ ephemeral: true })
+
+  // TODO: Cache these values
+  const guildCount =
+    (await client.shard?.fetchClientValues('guilds.cache.size')) ?? client.guilds.cache.size
+  const memberCount =
+    (await client.shard?.fetchClientValues('users.cache.size')) ?? client.users.cache.size
+
+  const roleGroupsCount = await prisma.roleGroups.count()
+  const roleButtonsCount = await prisma.roleGroups.count()
+
+  await interaction.editReply({
+    content: '\u200B',
+    embeds: [
+      {
+        author: {
+          name: 'Role Buttons',
+          icon_url: client.user?.displayAvatarURL({ forceStatic: true })
+        },
+        description:
+          'Role Button bot lets your server members choose roles with customizable buttons!',
+        color: 0xffffff,
+
+        fields: [
+          {
+            name: 'Using the bot',
+            value: stripIndent`
+              **1.** Create a role group with **\`/creategroup\`**
+              **2.** Add roles to the role group with **\`addrole\`**
+              **3.** Display the role group using **\`/display\`**
+            `
+          },
+          {
+            name: '📊 Live Statistics',
+            value: stripIndent`
+              • Serving ${memberCount} users in ${guildCount} servers
+              • Tracking ${roleButtonsCount} buttons assigned to ${roleGroupsCount} groups
+            `,
+            inline: true
+          },
+          {
+            name: '🖥️ Host Statistics',
+            value: stripIndent`
+              • Running ${os.cpus()[0].model} with ${os.cpus().length} threads
+              • ${Math.round(os.totalmem() / 1024 / 1024 / 1024)} GB of RAM (Using ${(
+              (os.totalmem() - os.freemem()) /
+              1024 /
+              1024 /
+              1024
+            ).toPrecision(2)} GB)
+            `,
+            inline: true
+          },
+          {
+            name: 'ℹ Extra Info',
+            value: stripIndent`
+              • [Use the online dashboard for easier configuration](https://rolebuttons.angelo.fyi) (Work In Progress!)
+              • [Support the creator here](https://angeloanan.xyz)
+            `,
+            inline: false
+          }
+        ]
+      }
+    ]
   })
 }
 
