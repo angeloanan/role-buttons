0f0f9c97f83bb9319c15e2dbe4f42cbdc2ee18d6
Christopher Angelo <angelo@angeloanan.xyz>
2022-02-20 03:59:57 +0700

    feat(guildCreateEvent): trycatch

--- bot/events/guildCreate.ts
+++ bot/events/guildCreate.ts
@@ -3,11 +3,18 @@ import type { Guild } from 'discord.js'
 
 const handler = async (guild: Guild) => {
   // Create guild on first join
-  await prisma.guild.create({
-    data: {
-      id: guild.id
-    }
-  })
+  try {
+    await prisma.guild.create({
+      data: {
+        id: guild.id
+      }
+    })
+  } catch (e) {
+    console.error(
+      'Failed to create guild data on Database. Is the guild already in the database?',
+      e
+    )
+  }
 }
 
 export default handler
