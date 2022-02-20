5c28aea435eb70a67a22195a4779bdb1af715056
Christopher Angelo <angelo@angeloanan.xyz>
2022-02-20 10:05:58 +0700

    refactor(db): remove duplicate global declaration

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,8 @@
 /* eslint-disable no-var */
-import type { InfluxDB } from '@influxdata/influxdb-client'
 import { PrismaClient } from '@prisma/client'
 
 declare global {
   var prisma: PrismaClient | undefined
-  var influx: InfluxDB | undefined
 }
 
 export const prisma =
