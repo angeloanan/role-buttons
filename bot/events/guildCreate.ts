607ac723c9bf320e4a92c64b4f12316ed1a8917e
Christopher Angelo <angelo@angeloanan.xyz>
2022-02-20 11:22:21 +0700

    feat: log guild count

--- bot/events/guildCreate.ts
+++ bot/events/guildCreate.ts
@@ -1,5 +1,7 @@
-import { prisma } from 'db'
+import { influx, prisma } from 'db'
 import type { Guild } from 'discord.js'
+
+import { client } from '../index.js'
 
 const handler = async (guild: Guild) => {
   // Create guild on first join
@@ -9,6 +11,14 @@ const handler = async (guild: Guild) => {
         id: guild.id
       }
     })
+
+    const guildCount = (
+      ((await client.shard?.fetchClientValues('guilds.cache.size')) as number[]) ?? [
+        client.guilds.cache.size
+      ]
+    ).reduce((p, c) => p + c, 0)
+
+    influx.guildCountLog(guildCount)
   } catch (e) {
     console.error(
       'Failed to create guild data on Database. Is the guild already in the database?',
--- bot/events/guildDelete.ts
+++ bot/events/guildDelete.ts
@@ -1,12 +1,29 @@
-import { prisma } from 'db'
+import { influx, prisma } from 'db'
 import type { Guild } from 'discord.js'
 
+import { client } from '../index.js'
+
 const handler = async (guild: Guild) => {
-  await prisma.guild.delete({
-    where: {
-      id: guild.id
-    }
-  })
+  try {
+    await prisma.guild.delete({
+      where: {
+        id: guild.id
+      }
+    })
+
+    const guildCount = (
+      ((await client.shard?.fetchClientValues('guilds.cache.size')) as number[]) ?? [
+        client.guilds.cache.size
+      ]
+    ).reduce((p, c) => p + c, 0)
+
+    influx.guildCountLog(guildCount)
+  } catch (e) {
+    console.error(
+      'Failed to delete guild data on Database. Is the guild already deleted in the database?',
+      e
+    )
+  }
 }
 
 export default handler
