52cc0017d0640374aa642c9a37dc7e3461f225af
Christopher Angelo <angelo@angeloanan.xyz>
2022-02-20 10:07:37 +0700

    fea: start logging pings

--- bot/events/ready.ts
+++ bot/events/ready.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // ^ Remove this later!
+import { influx } from 'db'
 import { OAuth2Scopes } from 'discord.js'
 
 import { client } from '../index.js'
@@ -13,6 +14,10 @@ const handler = () => {
       permissions: ['Administrator', 'SendMessages', 'ManageRoles']
     })
   )
+
+  setInterval(() => {
+    influx.gatewayPingLog(client.ws.ping)
+  }, 15000)
 }
 
 export default handler
